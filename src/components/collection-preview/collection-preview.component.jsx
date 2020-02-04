2e58aa9e1e2cf0e4499714eb66504147f2245d2b
xmksz3 <markus.sanderst@gmail.com>
2020-02-04 17:38:50 -0600

    added addItem reducer functionality to collectionItem

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
             {
                 items // potential lag in performance as functions are called each time component renders
                     .filter((item, idx) => idx < 4)
-                    .map(({ id, ...otherItemProps }) => (
-                        <CollectionItem key={id}  {...otherItemProps}/>
+                    .map((item) => (
+                        <CollectionItem key={item.id}  item={item}/>
                     ))
             }
         </div>
