634f3f485f0120e890942f671763b0e5f2802ac4
xmksz3 <markus.sanderst@gmail.com>
2020-02-04 15:26:37 -0600

    fixed typo in index.js, implemented userReducer and userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -7,24 +9,21 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    }
-  }
+class App extends React.Component {
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot( snapshot => {
-            this.setState({
+            setCurrentUser({
               currentUser: {
                 id: snapshot.id,
                 ...snapshot.data(),
@@ -32,7 +31,7 @@ export default class App extends React.Component {
             });
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -44,7 +43,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
@@ -55,4 +54,9 @@ export default class App extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
 
--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ ReactDOM.render(
     <Provider store={store} >
         <BrowserRouter >
             <App />
-        </BrowserRouter>, 
-    </Provider>
+        </BrowserRouter> 
+    </Provider>,
     document.getElementById('root')
 );
 
