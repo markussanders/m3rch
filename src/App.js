58c48872fccff2935f37433803f560444bbcc22e
xmksz3 <markus.sanderst@gmail.com>
2020-02-04 10:50:59 -0600

    created and styled sign in page with sign in w/ google components

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,8 +50,10 @@ class SignIn extends React.Component {
                         label='password'
                         required
                     />
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Signin with Google</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Signin with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         );
