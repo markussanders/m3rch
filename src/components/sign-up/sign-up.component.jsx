620ec0ccad7cdbe104526a34024992b94df149d6
xmksz3 <markus.sanderst@gmail.com>
2020-02-04 13:01:01 -0600

    implemented firebase utils, included ability to store authenticated users into firestore database

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -19,6 +19,42 @@ export default class SignUp extends React.Component {
         }
     }
 
+    handleSubmit = async event => {
+        event.preventDefault();
+        
+        const { displayName, email, password, confirmPassword } = this.state;
+
+        if (password !== confirmPassword) {
+            alert("passwords don't match");
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
+            
+            await createUserProfileDocument(user, { displayName });
+            
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
     render() {
         const { displayName, email, password, confirmPassword } = this.state;
         return (
